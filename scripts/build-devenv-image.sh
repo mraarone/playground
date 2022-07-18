ff88730565425055bd9df40db1a85f44cc6c2b91
mraarone <mraarone@yahoo.com>
2022-07-18 21:51:40 +0000

    Reorganized files, added script image build.

---
+++ scripts/build-devenv-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ $1 == "--push" ]; then
+    WILL_PUSH=1
+else
+    WILL_PUSH=0
+fi
+
+docker buildx build \
+    --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 \
+    -t "$GITHUB_REPOSITORY-devenv:latest" \
+    $( (( $WILL_PUSH == 1 )) && printf %s '--push' ) \
+    ../Dockerfiles/dockerfile.devenv
