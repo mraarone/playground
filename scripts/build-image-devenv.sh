893ba03e5f5ed99fc655908dde1482e12b77b249
mraarone <mraarone@yahoo.com>
2022-07-18 23:00:08 +0000

    Updated build-image-devenv.sh

--- scripts/build-image-devenv.sh
+++ scripts/build-image-devenv.sh
@@ -11,8 +11,10 @@ else
     WILL_PUSH=0
 fi
 
-docker buildx build \
-    -t "$GITHUB_REPOSITORY-devenv:latest" \
-    $( (( $WILL_PUSH == 1 )) && printf %s '--push' ) \
-    -f dockerfiles/Dockerfile.devenv \
-    .
+docker login -u $SECRETS_DOCKER_USER -p $SECRETS_DOCKER_PASS
+
+#docker buildx build \
+#    -t "$GITHUB_REPOSITORY-devenv:latest" \
+#    $( (( $WILL_PUSH == 1 )) && printf %s '--push' ) \
+#    -f dockerfiles/Dockerfile.devenv \
+#    .
