0a1ed1bba40605adb0088039be2d3c3aaa86e7f7
mraarone <mraarone@yahoo.com>
2022-07-18 22:18:40 +0000

    Updated dockerfile.

--- scripts/build-image-devenv.sh
+++ scripts/build-image-devenv.sh
@@ -12,7 +12,6 @@ else
 fi
 
 docker buildx build \
-    --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 \
     -t "$GITHUB_REPOSITORY-devenv:latest" \
     $( (( $WILL_PUSH == 1 )) && printf %s '--push' ) \
     -f dockerfiles/Dockerfile.devenv \
