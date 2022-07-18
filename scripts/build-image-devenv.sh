bd029f961bbc9cc9aa07bfe9e8e285c21bdc1991
mraarone <mraarone@yahoo.com>
2022-07-18 23:09:23 +0000

    Updated build image.

--- scripts/build-image-devenv.sh
+++ scripts/build-image-devenv.sh
@@ -11,7 +11,8 @@ else
     WILL_PUSH=0
 fi
 
-echo $SECRETS_DOCKER_PASS | docker login -u $SECRETS_DOCKER_USER --password-stdin
+echo ${{ secrets.SECRETS_DOCKER_PASS }} | docker login --username ${{ secrets.SECRETS_DOCKER_USER }} --password-stdin docker.io
+# echo $SECRETS_DOCKER_PASS | docker login -u $SECRETS_DOCKER_USER --password-stdin
 
 #docker buildx build \
 #    -t "$GITHUB_REPOSITORY-devenv:latest" \
