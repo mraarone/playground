fe3065ec84a4f05e3b37e67538b4e705f791b3a2
Aaron Elliott <mraarone@yahoo.com>
2022-07-24 01:12:20 +0000

    Reconfigured docker workflow script.

--- scripts/build-image-devenv.sh
+++ scripts/build-image-devenv.sh
@@ -11,15 +11,14 @@ else
     WILL_PUSH=0
 fi
 
-echo $SECRETS_DOCKER_PASS | docker login -u $SECRETS_DOCKER_USER --password-stdin docker.io
+# Docker Hub
+# echo $SECRETS_DOCKER_PASS | docker login -u $SECRETS_DOCKER_USER --password-stdin docker.io
+
+# GitHub Container Repository (ghcr.io)
+echo $SECRETS_GITHUB_TOKEN | docker login -u $SECRETS_GITHUB_USER --password-stdin ghcr.io
 
 docker buildx build \
     -t "$GITHUB_REPOSITORY-devenv:$(date +%s)" \
-    $( (( $WILL_PUSH == 1 )) && printf %s '--push' ) \
-    -f dockerfiles/Dockerfile.devenv \
-    .
-
-docker buildx build \
     -t "$GITHUB_REPOSITORY-devenv:latest" \
     $( (( $WILL_PUSH == 1 )) && printf %s '--push' ) \
     -f dockerfiles/Dockerfile.devenv \
